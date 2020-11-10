a3ad9c29f8b770497ce5278120f39bc19aa4eb31
benjamin.oluyomi <boluyomi@academytrainee.com>
2020-11-10 17:12:47 +0000

    calculator scanner

---
+++ srcmain/javabeginner/Scanners/Main.java
@@ -0,0 +1,62 @@
+package Scanners;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		//Excersise 1
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter the first number");
+		
+		int num1 = scan.nextInt();
+		
+		Scanner scan2 = new Scanner(System.in);
+		System.out.println("Enter the second number");
+		
+		int num2 = scan2.nextInt();
+		
+		Scanner op = new Scanner(System.in);
+		System.out.println("Enter the operator");
+		String operator = op.next();
+		
+		if(operator.equals("+")) {
+			
+			System.out.println(add(num1, num2));
+			
+		} else if(operator.equals("-")) {
+			System.out.println(sub(num1, num2));
+			
+		} else if(operator.equals("*")) {
+			System.out.println(mult(num1, num2));
+			
+		} else if(operator.equals("/")) {
+			System.out.println(div(num1, num2));
+			
+		}
+		
+	}
+	
+	
+	public static int add(int a, int b) {
+		int result = a+b;
+		
+		return result;
+	}
+	public static int sub(int a, int b) {
+		int result = a -b;
+		
+		return result;
+	}
+	public static int mult(int a, int b) {
+		int result = a *b;
+	
+		return result;
+	}
+	public static double div(int a, int b) {
+		double result = a/b;
+		
+		return result;
+	}
+
+}
--- srcmain/javabeginner/classes/Wild.java
+++ srcmain/javabeginner/classes/Wild.java
@@ -1,15 +1,14 @@
 package classes;
 
-public class Wild extends Dog{
+public class Wild extends Animal{
 	//NEW ATTRIBUTE
 	Boolean isDang;
-	public Wild(boolean onLand, int legnum, String name, String noise,boolean isDang) {
+	public Wild(boolean onLand, int legnum, String name, boolean isDang) {
 		//INHERITANCE
-		super(onLand, legnum, name,noise);
+		super(onLand, legnum, name);
 		this.onLand = onLand;
 		this.legnum = legnum;
 		this.name = name;
-		this.noise = noise;
 		this.isDang= isDang;
 	}
 	
@@ -19,11 +18,10 @@ public class Wild extends Dog{
 		public boolean getDanger() {
 			return isDang;
 		}
-		public void Method(boolean onLand, int legnum, String name, String noise, boolean isDang) {
+		public void Method(boolean onLand, int legnum, String name, boolean isDang) {
 			this.onLand = onLand;
 			this.legnum = legnum;
 			this.name = name;
-			this.noise = noise;
 			this.isDang = isDang;
 		}
 		
